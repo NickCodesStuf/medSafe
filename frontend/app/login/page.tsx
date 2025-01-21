987ae5e8e3c57f395b42b6b776b0497927229c14
N3rminal <88295578+N3rminal@users.noreply.github.com>
2025-01-21 15:17:35 -0600

    Added login page

---
+++ frontend/app/login/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <div>
+    <h1 className="font-bold">Login</h1>
+      <p>Log in to your account</p>
+    </div>
+  );
+}
